cb4db5460e7bbc3d515d0ba067b36bfa5fd4e1fb
Chienweichih <chienweichih@gmail.com>
2015-05-21 22:46:56 +0800

    Finish Git toturial * add -> commit -> push * pull
    * git config --global user.name
    * git config --global user.email
    * git config --global core.editor
    
    * git init
    
    * git status

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ res = requests.get('http://weather.tp.edu.tw/school/school.html', params=payload
 
 print res.text.encode("utf-8")
 print "Hello! Nice to meet U."
+print "Hello! You can GIT now!!"
+
+print "add -> commit -> push"
+print "pull"
