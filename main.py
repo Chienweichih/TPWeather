8ee303cd22ee601d3e788ddfcb31ce3952436ea9
yuyanlo <rita80_kk@hotmail.com.tw>
2015-05-22 00:22:16 +0800

    Encoding problem

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
 import requests
+import codecs
+import json
 
 payload = {'SchoolID':'383606'}
 res = requests.get('http://weather.tp.edu.tw/school/school.html', params=payload)
-
-print res.text.encode("utf-8")
-print "Hello! Nice to meet U."
-print "Hello! You can GIT now!!"
-
-print "add -> commit -> push"
-print "pull"
-
-
-print "Thanks for my dear. :)"
-
-while True:
-    print "my love"
+res.encoding = "utf-8"
+print res.text
