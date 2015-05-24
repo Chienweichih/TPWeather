b5a8b3df68da15da3fd4043d8f92ea32f8376846
Chienweichih <chienweichih@gmail.com>
2015-05-24 12:28:13 +0800

    reorganize by used def():

--- main.py
+++ main.py
@@ -6,54 +6,58 @@ import json
 import codecs
 
 '''''''''''''''''''''
-* Time Setting
-'''''''''''''''''''''
-
-startTimeStr = '2015-05-20'
-endTimeStr = '2015-05-21'
-timeFilter = '%Y-%m-%d'
-
-startTime = datetime.datetime.strptime(startTimeStr,timeFilter)
-endTime = datetime.datetime.strptime(endTimeStr,timeFilter)
-
-startTimeStamp = time.mktime(startTime.timetuple()) * 1000
-endTimeStamp = time.mktime(endTime.timetuple()) * 1000
-
-'''''''''''''''''''''
 * School Id
 '''''''''''''''''''''
-schoolID = {u'至善國中':'413504',u'市大附小':'353608',u'國語實小':'353604',
-            u'博愛國小':'323609',u'老松國小':'373609',u'指南國小':'383612',
-            u'光復國小':'323603',u'福林國小':'413603',u'中正國小':'343602',
-            u'龍安國小':'333601',u'三玉國小':'413619',u'吳興國小':'323606',
-            u'五常國小':'343607',u'逸仙國小':'423602',u'湖田國小':'423605'}
+schoolID = {u'至善國中':'413504',u'國語實小':'353604',u'福林國小':'413603',
+            u'光復國小':'323603',u'大龍國小':'363607',u'五常國小':'343607',
+            u'吳興國小':'323606',u'市大附小':'353608',u'博愛國小':'323609',
+            u'老松國小':'373609',u'指南國小':'383612',u'逸仙國小':'423602',
+            u'三玉國小':'413619',u'湖田國小':'423605',u'中正國小':'343602',
+            u'龍安國小':'333601'}
 
-'''''''''''''''''''''
-* Get requests
-'''''''''''''''''''''
-payload = {'id'         : schoolID[u'至善國中'],
-           'by'         : 'minute',
-           'start'      : str(startTimeStamp),
-           'end'        : str(endTimeStamp)}
-res = requests.get('http://weather.tp.edu.tw/Ajax/jsonp/table.ashx', params=payload)
+def getTimeStamp(startTimeStr, endTimeStr, timeFilter):
+    startTime = datetime.datetime.strptime(startTimeStr,timeFilter)
+    endTime = datetime.datetime.strptime(endTimeStr,timeFilter)
+    
+    startTimeStamp = time.mktime(startTime.timetuple()) * 1000
+    endTimeStamp = time.mktime(endTime.timetuple()) * 1000
+    
+    return (startTimeStamp, endTimeStamp)
 
-'''''''''''''''''''''
-* JSON Operate
-'''''''''''''''''''''
-data = json.loads(res.text)
-#print data['result'][-1][u'開始時間']
+def getRequests(schoolID, startTimeStamp, endTimeStamp):
+    payload = {'id'     : schoolID,
+               'by'     : 'minute',
+               'start'  : str(startTimeStamp),
+               'end'    : str(endTimeStamp)}
+    res = requests.get('http://weather.tp.edu.tw/Ajax/jsonp/table.ashx', params=payload)
+    return res
 
-def writeFile(f,s):
-    if isinstance(s, str) or isinstance(s, unicode):
-        f.write(s)
+def checkBeforeWrite(myFile, string):
+    if isinstance(string, str) or isinstance(string, unicode):
+        myFile.write(string)
     else:
-        f.write(str(s))
+        myFile.write(str(string))
+        
+def writeToFile(fileName, data):
+    with codecs.open(fileName, "w", "utf-8") as output:
+        for result in data:
+            for key in result:
+                checkBeforeWrite(output, key)
+                output.write(" : ")
+                checkBeforeWrite(output, result[key])
+                output.write("\n")
+            output.write("=====================================================\n")
 
-with codecs.open("output.txt","w","utf-8") as file:
-        for result in data['result']:
-                for key in result:
-                    writeFile(file,key)
-                    file.write(" : ")
-                    writeFile(file,result[key])
-                    file.write("\n")
-                file.write("=====================================================\n")
+def main():
+    startTimeStr = '2015-05-20'
+    endTimeStr = '2015-05-21'
+    timeFilter = '%Y-%m-%d'
+
+    startTimeStamp, endTimeStamp = getTimeStamp(startTimeStr, endTimeStr, timeFilter)
+    
+    res = getRequests(schoolID[u'至善國中'], startTimeStamp, endTimeStamp)
+    data = json.loads(res.text) #JSON Operate
+    writeToFile("output.txt", data['result'])
+
+if __name__ == "__main__":
+    main()
